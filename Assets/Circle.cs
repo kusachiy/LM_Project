5335810c9d6a24910b476b6a0ff5134779799212
Vlad Gomylev <dead0vlad@gmail.com>
2018-04-20 18:58:56 +0300

    поправлена крутилка

--- Assets/Circle.cs
+++ Assets/Circle.cs
@@ -9,74 +9,72 @@ public class Circle : MonoBehaviour
     public GameObject[] Interactors;
     public GameObject Controller;
     public float Sensivity = 5f;
+    public float Distance = 0.1f;
+    public float FreeRotationDrag = 0.01f;
+    public float HandDrag = 2f;
+
     private LeapServiceProvider _leap;
     private MeshRenderer _meshRenderer;
-    public float Distance = 0.1f;
-    private bool _handIsConnected = false;
-    private Hand _hand;
     private Vector3 _previosHandRotation;
-    private Vector2 _previosPos;
-  
-	// Use this for initialization
-	void Start () {
+    private Rigidbody _rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
         _leap = Controller.GetComponent<LeapServiceProvider>();
         _meshRenderer = GetComponent<MeshRenderer>();
-        _previosPos = Vector2.right;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void FixedUpdate()
     {
         CheckHands();
-        if (_handIsConnected)
-            RotateObject();
     }
     void CheckHands()
     {
         var frame = _leap.CurrentFrame;
+        _rigidbody.angularDrag = FreeRotationDrag; 
+        _meshRenderer.material.color = Color.black;
         foreach (var hand in frame.Hands)
         {
             foreach (var finger in hand.Fingers)
             {
                 if (Vector3.Distance(finger.TipPosition.ToVector3(), transform.position) < Distance)
                 {
-                    _meshRenderer.material.color = Color.red;
                     Debug.DrawLine(transform.position, finger.TipPosition.ToVector3(), Color.red);
-                    _handIsConnected = true;
-                    _hand = hand;
-                    return;
+                    RotateObject(hand);
+                    _meshRenderer.material.color = Color.red;
+                    _rigidbody.angularDrag += HandDrag;
+                    break;
                 }
-                _meshRenderer.material.color = Color.black;
-                _handIsConnected = false;
             }
         }
     }
-    void RotateObject()
+    void RotateObject(Hand hand)
     {
-        if (float.IsNaN(_hand.PalmPosition.x)|| float.IsNaN(_hand.PalmPosition.y)|| float.IsNaN(_hand.PalmPosition.z))
-        {
-            Debug.Log("NAN SECURE OK");
-            return;
-        }
-        var offset = _hand.PalmVelocity.ToVector3()* Sensivity;
-        var position = new Vector2(_hand.PalmPosition.x - transform.position.x, _hand.PalmPosition.z - transform.position.z);
-        var direction = Vector2.SignedAngle(position,_previosPos) >0?1:-1;//1 или -1
+        var offset = hand.PalmVelocity.ToVector3() * Sensivity;
+        var position = new Vector2(hand.PalmPosition.x - transform.position.x, hand.PalmPosition.z - transform.position.z);
+        var prevPosition = new Vector2(position.x - hand.PalmVelocity.x, position.y - hand.PalmVelocity.z);
+        var direction = Vector2.SignedAngle(position, prevPosition) > 0 ? 1 : -1;//1 или -1
         var chord = Mathf.Sqrt(Mathf.Pow(offset.x, 2) + Mathf.Pow(offset.z, 2));
-        var radius = Vector3.Distance(transform.position, _hand.PalmPosition.ToVector3());
-        
-        var rotateAngle = Mathf.Asin(Mathf.Deg2Rad * chord / (2 * radius))*2 * Mathf.Rad2Deg * direction;
-        if (float.IsNaN(rotateAngle)) 
+        var radius = Vector3.Distance(transform.position, hand.PalmPosition.ToVector3());
+
+        var rotateAngle = Mathf.Asin(Mathf.Deg2Rad * chord / (2 * radius)) * 2 * Mathf.Rad2Deg * direction;
+        if (float.IsNaN(rotateAngle))
         {
             Debug.Log("Angle is NaN");
             return;
         }
-        _previosPos = position;
-        //Debug.Log(angle);
-
-        transform.Rotate(0, rotateAngle, 0);
+        _rigidbody.AddTorque(Vector3.up * rotateAngle,ForceMode.Acceleration);
+        //transform.Rotate(0, rotateAngle, 0);
+    }
+    void AddForce()
+    {
     }
 
 }
